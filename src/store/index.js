4a896569f28f6ec6961dce9265d55325872f2088
Teun Kelting <teunkelting@hotmail.com>
2017-04-20 12:17:00 +0200

    Fixed getters import

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import * as getters from './getters'
+import getters from './getters'
 import * as actions from './actions'
 import * as mutations from './mutations'
 
