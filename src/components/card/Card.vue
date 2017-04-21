495eca7ad2c88008806df228a1945825573493e6
Loosie94 <mail@reneloos.nl>
2017-04-21 20:01:01 +0200

    CSS changes on card overview

--- src/components/card/Card.vue
+++ src/components/card/Card.vue
@@ -2,14 +2,23 @@
   <div class="card" :class="normalizedCategory" @click="openPost">
     <header>
      <div class="inner-left">
-         <img class="avatar" src="https://placehold.it/80x80" :alt="post_data.user.username">
-         <span class="u_name">{{ post_data.user.username }}</span><span class="u_date">Eergisteren</span> 
+         <img class="avatar" src="https://placehold.it/60x60" :alt="post_data.user.username">
+         <div id="user_details">
+             <h4 class="u_name">{{ post_data.user.username }}</h4>
+             <h5 class="u_date">Eergisteren</h5> 
+         </div>
       </div>
       <div class="inner-right">
-          <span>{{ post_data.category.name }}</span>
+          <a href="#" class="button cat_button">{{ post_data.category.name }}</a>
       </div>
       </header>
-    <h2 class="title">{{ post_data.body }}</h2>
+      <section id="card-content">
+          <h2 class="title">{{ post_data.body }}</h2>
+      </section>
+      <footer>
+          <img src="https://placehold.it/20x20" alt="like"><span>23</span>
+          <img src="https://placehold.it/20x20" alt="comment"><span>12</span>
+      </footer>
   </div>
 </template>
 
