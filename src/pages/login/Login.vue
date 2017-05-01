f8252f141db84261a85f5acb933bf11dff24a628
Loosie94 <mail@reneloos.nl>
2017-05-01 10:52:19 +0200

    Create post with voice styling

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="page--login">
-
     <div class="login-container">
       <img class="logo" src="./../../assets/images/logo/white.svg">
       <button class="btn" v-on:click="login()">Inloggen</button>
     </div>
-
   </div>
 </template>
-
 <script src="./Login.vue.js"></script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss" src="./Login.vue.scss"></style>
