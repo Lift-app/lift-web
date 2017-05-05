2e71c943f78bef72b4774fc627624b5197c74f7a
Loosie94 <mail@reneloos.nl>
2017-05-05 11:27:52 +0200

    Added responsive font sizes, some css changes

--- src/components/card/Card.vue
+++ src/components/card/Card.vue
@@ -4,11 +4,11 @@
 
     <header>
       <div class="user">
-        <img class="avatar" src="https://placehold.it/60x60">
+        <img class="avatar" src="https://placeimg.com/60/60/people">
         <div class="user-details">
-          <h4 class="u-name" v-if="post_data.user">{{ post_data.user.username }}</h4>
-          <h4 class="u-name" v-else>Anoniem</h4>
-          <h5 class="u-date">Eergisteren</h5>
+          <h3 class="u-name" v-if="post_data.user">{{ post_data.user.username }}</h3>
+          <h3 class="u-name" v-else>Anoniem</h3>
+          <h4 class="u-date">Eergisteren</h4>
         </div>
       </div>
       <a href="#" class="btn cat-btn">{{ post_data.category.name }}</a>
@@ -16,18 +16,17 @@
 
     <section>
       <h2 class="title" v-if="post_data.type !== 'audio'">{{ post_data.body }}</h2>
-      <audio style="width: 100%; margin: 20px 0;" preload="auto" v-else :src="post_data.body" controls></audio>
+      <audio style="width: 100%; margin: 20px 0;" preload="auto" v-else :src="post_data.body" class="audio-post" controls></audio>
     </section>
 
     <footer>
       <div class="footer-action">
-        <button class="like" aria-label="Vraag leuk vinden"><img src="../../assets/images/icons/heart-white.svg" alt="Vind ik leuk"></button>
+        <button class="like" aria-label="Vraag leuk vinden"><img src="../../assets/images/icons/heart-white.svg" alt="Vind ik leuk!"></button>
         <span class="count like-count" v-bind:aria-label="post_data.likes">{{ post_data.likes }}</span>
       </div>
 
       <div class="footer-action">
-        <button class="comment" aria-label="Vraag leuk vinden"><img src="../../assets/images/icons/speech-bubble-white.svg" alt="Reageren op deze vraag"></button>
-
+        <button class="comment" aria-label="Reageren op deze vraag"><img src="../../assets/images/icons/speech-bubble-white.svg" alt="Reageren op deze vraag"></button>
         <span class="count comment-count" aria-label="15 reacties">15</span>
       </div>
     </footer>
