3968e2065c2259695eaf5fa67b923a0bd120df24
Steven Oud <soud@protonmail.com>
2017-05-20 17:19:41 +0200

    Fix duplicate eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,14 +22,12 @@ module.exports = {
     // enforce consistent spacing before function definition opening parenthesis
     'space-before-function-paren': 0,
     // Add a space after keywords
-    'keyword-spacing': 0,
+    'keyword-spacing': 1,
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-
-    'keyword-spacing': 1
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
 }
