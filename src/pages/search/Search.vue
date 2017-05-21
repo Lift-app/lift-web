f3c2c8f90db27cba4937b76be2a89456004e8629
Steven Oud <soud@protonmail.com>
2017-05-21 03:04:21 +0200

    Update search example to be less confusing

--- src/pages/search/Search.vue
+++ src/pages/search/Search.vue
@@ -9,8 +9,8 @@
           <img src="../../assets/images/icons/search-lift-white.svg" class="search-icon" alt="Zoeken"/>
           <h3 class="search-title">Waar wil je naar zoeken?</h3>
           <div class="search-wrapper">
-            <!--<v-select class="search-bar" label="name" v-model="category" :options="categories" placeholder="Bijvoorbeeld 'Liefde'"></v-select>-->
-            <input type="text" class="search-bar" placeholder="Bijvoorbeeld 'Liefde'" v-model="keyword">
+            <!--<v-select class="search-bar" label="name" v-model="category" :options="categories" placeholder="Bijvoorbeeld 'kinderen'"></v-select>-->
+            <input type="text" class="search-bar" placeholder="Bijvoorbeeld 'kinderen'" v-model="keyword">
             <button type="submit" class="search-submit">Zoeken</button>
           </div>
         </div>
