df7187750d7291228be3573c146ae565418bbb9f
Teun Kelting <teunkelting@hotmail.com>
2017-05-23 15:35:55 +0200

    onboarding styling

--- src/pages/onboarding/Onboarding.vue
+++ src/pages/onboarding/Onboarding.vue
@@ -2,7 +2,7 @@
   <div class="page--onboarding">
     <swiper :options="swiperOption" ref="swiper">
       <swiper-slide class="first">
-        <img class="background" src="../../assets/images/icons/onboarding/onboarding-bg.svg" alt="achtergrond">
+        slide
       </swiper-slide>
       <swiper-slide>
         slide
@@ -19,6 +19,19 @@
       <button class="swiper-button-next" slot="button-next"><img src="../../assets/images/icons/onboarding/onboarding-arrow-next.svg" alt="Vorige"></button>
       <!--<div class="swiper-scrollbar"   slot="scrollbar"></div>-->
     </swiper>
+
+    <svg class="onboarding-background-container" viewBox="0 0 530 561" xmlns="http://www.w3.org/2000/svg" width="100%" height="470" preserveAspectRatio="none">
+      <defs>
+        <rect id="path-1" x="0" y="0" width="530" height="561"></rect>
+      </defs>
+      <g id="Design" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <mask id="mask-2">
+          <use xlink:href="#path-1"></use>
+        </mask>
+        <use id="Rectangle-12" fill="transparent" xlink:href="#path-1"></use>
+        <circle class="onboarding-background" fill="#50E3C2" mask="url(#mask-2)" cx="267" cy="127" r="404"></circle>
+      </g>
+    </svg>
   </div>
 </template>
 
