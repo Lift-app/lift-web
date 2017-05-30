772b26afaee3995661b29f5befde0b992fd5d05b
Mirko van der Waal <mirko.van.der.waal@holder.nl>
2017-05-30 13:31:06 +0200

    Updated player component

--- src/components/player/Player.vue
+++ src/components/player/Player.vue
@@ -3,10 +3,22 @@
     <div class="controls">
       <span class="buttons">
         <a role="button" class="button-play active" @click="switchState">
-          <img src="../../assets/images/icons/play-lift.svg">
+          <svg width="10px" height="12px" viewBox="0 0 10 12" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+              <g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                  <g class="play" id="speak-question-content-recording-copy" transform="translate(-96.000000, -213.000000)" fill="#FFFFFF">
+                      <path d="M105.65625,218.28125 C105.885418,218.468751 106,218.708332 106,219 C106,219.291668 105.885418,219.531249 105.65625,219.71875 L96.96875,224.90625 C96.8437494,224.96875 96.7291672,225 96.625,225 C96.4583325,225 96.3125006,224.937501 96.1875,224.8125 C96.0624994,224.687499 96,224.541667 96,224.375 L96,213.625 C96,213.458333 96.0624994,213.312501 96.1875,213.1875 C96.3125006,213.062499 96.4583325,213 96.625,213 C96.7500006,213 96.8645828,213.03125 96.96875,213.09375 L105.65625,218.28125 Z" id="ion-play---Ionicons"></path>
+                  </g>
+              </g>
+          </svg>
         </a>
         <a role="button" class="button-pause" @click="switchState">
-          <img src="../../assets/images/icons/pause-lift.svg">
+          <svg width="8px" height="12px" viewBox="0 0 8 12" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+              <g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                  <g class="pause" id="speak-question-content-recording-copy" transform="translate(-81.000000, -213.000000)" fill="#FFFFFF">
+                      <path d="M84,224.625 C84,224.875001 83.8750012,225 83.625,225 L81.375,225 C81.1249988,225 81,224.875001 81,224.625 L81,213.375 C81,213.124999 81.1249988,213 81.375,213 L83.625,213 C83.8750012,213 84,213.124999 84,213.375 L84,224.625 Z M88.625,213 C88.8750012,213 89,213.124999 89,213.375 L89,224.625 C89,224.875001 88.8750012,225 88.625,225 L86.375,225 C86.1249988,225 86,224.875001 86,224.625 L86,213.375 C86,213.124999 86.1249988,213 86.375,213 L88.625,213 Z" id="ion-pause---Ionicons"></path>
+                  </g>
+              </g>
+          </svg>
         </a>
       </span>
       <span class="progress">
@@ -19,15 +31,10 @@
           type="range">
       </span>
       <span class="time">
-        <span class="current-time">0:01</span>
-        <span class="max-time">0:05</span>
+        <span class="max-time">0:00</span>
       </span>
     </div>
-    <audio
-      @timeupdate="updateState"
-      @ended="resetState"
-      preload="none"
-      :src="url">
+    <audio @timeupdate="updateState" @canplay="initState" @ended="resetState" :src="data.body">
     </audio>
   </div>
 </template>
--- src/components/player/Player.vue.js
+++ src/components/player/Player.vue.js
@@ -1,98 +1,91 @@
 export default {
   name: 'player',
   props: {
-    url: {}
+    data: {}
   },
-  mounted() {
-    this.$ = {
-      id: { 'TAudio': 'audio'
-          , 'BPlay': '.button-play'
-          , 'BPause': '.button-pause'
-          , 'PBar': '.progress-bar'
-          , 'CTime': '.current-time'
-          , 'MTime': '.max-time'
-      },
-      fn: {
-        timestamp: (number) => {
-          let mod = number % 60
-          return `${Math.floor(number / 60)}:${mod < 10 ? `0${mod}` : mod}`
-        },
+  data() {
+    return {
+      id: {
+        'Progress': '.progress-bar',
+        'Duration': '.max-time',
+        'Pause':    '.button-pause',
+        'Play':     '.button-play',
+        'Audio':    'audio'
       },
       el: {},
     }
   },
   methods: {
+    _pop(object) {
+      let key = Object.keys(object).pop()
+      return {
+        key: key,
+        value: object[key],
+        remainder: (delete object[key], object)
+      }
+    },
+    _controls(accumulator, container, object) {
+      let item = this._pop(object)
+      accumulator[item.key] = container.querySelector(item.value)
+      return Object.keys(object).length
+        ? this._controls.apply(null, [accumulator, container, item.remainder])
+        : accumulator
+    },
+    _container(node) {
+      return node.classList.contains('player')
+        ? node
+        : this._container(node.parentElement)
+    },
+    _meta(event) {
+      return this._controls({}, this._container(event.currentTarget || event), JSON.parse(JSON.stringify(this.id)))
+    },
+    _duration(controls) {
+      return Math.floor(controls.Audio.duration)
+    },
+    _current(controls) {
+      return Math.floor(controls.Audio.currentTime)
+    },
+    _timestamp(number) {
+      let mod = number % 60
+      return `${Math.floor(number / 60)}:${mod < 10 ? `0${mod}` : mod}`
+    },
+    _switch(controls) {
+      controls.Audio.paused ? controls.Audio.play() : controls.Audio.pause()
+      controls.Pause.classList.toggle('active')
+      controls.Play.classList.toggle('active')
+    },
     haltAction(event) {
-      // Using the Player doesn't open the card
       event.stopImmediatePropagation();
     },
+    initState(event) {
+      let controls = this._meta(event)
+      let current  = this._current(controls)
+      let duration = this._duration(controls)
+      controls.Duration.innerHTML = this._timestamp(duration)
+      controls.Progress.value     = current
+      controls.Progress.max       = duration
+    },
     switchState(event) {
-      // Pop a {key:value} pair off of a object. The return object
-      // consists of the key and value pair, as well as the remainder.
-      const pop = (object) => {
-        let key = Object.keys(object).pop()
-        return {
-          key: key,
-          value: object[key],
-          remainder: (delete object[key], object)
-        }
-      }
-
-      const controls = (accumulator, container, object) => {
-        let item = pop(object)
-        accumulator[item.key] = container.querySelector(item.value)
-        return Object.keys(object).length
-          ? controls.apply(null
-            , [ accumulator
-              , container
-              , item.remainder])
-          : accumulator
-      }
-
-      // Determine the relevant audio container and controls by recursively
-      // looking for our parent.
-      const container = (node) => {
-          return node.classList.contains('player')
-            ? node
-            : container(node.parentElement)
-      }
-
-      Object.assign( this.$.el
-                   , controls( {}
-                             , container(event.target)
-                             , this.$.id))
-
-      this.$.el.TAudio.paused
-        ? this.$.el.TAudio.play()
-        : this.$.el.TAudio.pause()
-
-      this.$.el.BPause.classList.toggle('active')
-      this.$.el.BPlay.classList.toggle('active')
+      let controls = this._meta(event)
+      this._switch(controls)
     },
-    updateState(_event) {
-      const CTime = Math.floor(this.$.el.TAudio.currentTime),
-            MTime = Math.floor(this.$.el.TAudio.duration)
-
-      console.log(CTime, MTime)
-
-      this.$.el.CTime.innerHTML = this.$.fn.timestamp(CTime)
-      this.$.el.MTime.innerHTML = this.$.fn.timestamp(MTime)
-      this.$.el.PBar.value      = CTime
-
-      // Move this to a initState.
-      this.$.el.PBar.max        = MTime
-      this.$.el.PBar.min        = 0
-      this.$.el.PBar.step       = 1
+    updateState(event) {
+      let controls = this._meta(event)
+      let current  = this._current(controls)
+      let duration = this._duration(controls)
+      controls.Duration.innerHTML = this._timestamp(duration)
+      controls.Progress.value     = current
     },
-    resetState(_event) {
-      const MTime = Math.floor(this.$.el.TAudio.duration)
-
-      this.$.el.BPlay.classList.add('active')
-      this.$.el.BPause.classList.remove('active')
-      this.$.el.MTime.innerHTML = this.$.fn.timestamp(MTime)
+    resetState(event) {
+      let controls = this._meta(event)
+      let duration = this._duration(controls)
+      controls.Play.classList.add('active')
+      controls.Pause.classList.remove('active')
+      controls.Duration.innerHTML = this._timestamp(duration)
     },
-    progressBarUpdate(_event) {
-      this.$.el.TAudio.currentTime = this.$.el.PBar.value
+    progressBarUpdate(event) {
+      let controls = this._meta(event)
+      controls.Audio.currentTime = controls.Progress.value
     }
   }
 }
