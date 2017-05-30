d6e76cb59685f66196f96a1edbfe33c0e782c934
Teun Kelting <teunkelting@hotmail.com>
2017-05-30 15:20:24 +0200

    Finished up onboarding

--- src/pages/onboarding/Onboarding.vue.js
+++ src/pages/onboarding/Onboarding.vue.js
@@ -8,15 +8,19 @@ export default {
       colors: ['#C783FF', '#50E3C2', '#90DBFF', '#FF5537'],
       swiperOption: {
         notNextTick: true,
-        grabCursor : true,
-        setWrapperSize :true,
-        pagination : '.swiper-pagination',
-        paginationClickable :true,
-        prevButton:'.swiper-button-prev',
-        nextButton:'.swiper-button-next',
-        mousewheelControl : true,
+        grabCursor: true,
+        setWrapperSize: true,
+        parallax: true,
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        prevButton: '.swiper-button-prev',
+        nextButton: '.swiper-button-next',
+        mousewheelControl: true,
         onTransitionStart(swiper) {
           vue.changeThemeColor(swiper)
+        },
+        onTransitionEnd(swiper) {
+          vue.handleSlideChange(swiper)
         }
       }
     }
@@ -25,11 +29,45 @@ export default {
     onboardingBgElem() {
       return document.querySelector('.onboarding-background')
     },
+    onboardingWorldElem() {
+      return document.querySelector('.onboarding-world')
+    },
+    onboardingPaginationElem() {
+      return document.querySelector('.swiper-pagination')
+    },
     onboardingBulletElems() {
       return document.querySelectorAll('.swiper-pagination-bullet')
+    },
+    onboardingFinishElem() {
+      return document.querySelector('.swiper-button-finish')
     }
   },
   methods: {
+
+    handleSlideChange(swiper) {
+      console.log(swiper.activeIndex)
+      switch(swiper.activeIndex) {
+        case 2:
+          this.onboardingWorldElem.classList.add('active')
+          this.onboardingFinishElem.classList.remove('active')
+          this.onboardingPaginationElem.classList.remove('hidden')
+          break
+        case 3:
+          this.onboardingWorldElem.classList.remove('active')
+          this.onboardingPaginationElem.classList.add('hidden')
+          this.onboardingFinishElem.classList.add('active')
+          break
+        case 4:
+          this.onboardingPaginationElem.classList.add('hidden')
+
+          break
+        default:
+          this.onboardingWorldElem.classList.remove('active')
+          this.onboardingPaginationElem.classList.remove('hidden')
+          this.onboardingFinishElem.classList.remove('active')
+      }
+    },
+
     changeThemeColor(swiper) {
       for (let i = 0; i < this.onboardingBulletElems.length; i++) {
         this.onboardingBulletElems[i].style.backgroundColor = '#849199' // default background color ($regent)
