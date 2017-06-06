0daa1572179b94e1133d115dc71a73e051e54bcf
Steven Oud <soud@protonmail.com>
2017-06-06 13:22:39 +0200

    Fix profile link :fire:

--- src/components/navigation/Navigation.vue.js
+++ src/components/navigation/Navigation.vue.js
@@ -2,6 +2,16 @@ import auth from '@/auth'
 
 export default {
   name: 'navigation',
+  data() {
+    return {
+      current_user: ''
+    }
+  },
+  watch: {
+    '$route' (to, from) {
+      this.current_user = localStorage.username
+    }
+  },
   computed: {
     current_user() {
       return localStorage.username
