689483b3c86762333dad28315777f995c610d38d
Teun Kelting <teunkelting@hotmail.com>
2017-06-15 11:07:40 +0200

    Added backRoute to popular

--- src/components/navigation/Navigation.vue
+++ src/components/navigation/Navigation.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navigation">
     <router-link :to="{name: 'VoorJou', params: {backRoute: this.$route}}" exact id="feed-nav" :class="$route.name === 'Post' ? 'router-link-active' : ''">Voor jou</router-link>
-    <router-link :to="{name: 'Popular'}" id="interests-nav">Populair</router-link>
+    <router-link :to="{name: 'Popular', params: {backRoute: this.$route}}" id="interests-nav">Populair</router-link>
     <router-link :to="{name: 'CreatePost', params: {backRoute: this.$route}}" id="add_post-nav">Iets zeggen</router-link>
     <router-link :to="{name: 'Search', params: {backRoute: this.$route}}" id="search-nav">Zoeken</router-link>
     <router-link :to="{name: 'Profile', params: {username: current_user, backRoute: this.$route}}" id="profile-nav">Profiel</router-link>
